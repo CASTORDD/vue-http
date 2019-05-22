b8139a37ff152584db8fdf7038e9bf0e64b1a99b
castordd <castord.d@gmail.com>
2019-05-22 10:51:18 -0300

    Conectando Axios con la API ( jsonServer )
    Conexion de axios con el servidor y creacion de un registro

--- src/components/TarefasLista.vue
+++ src/components/TarefasLista.vue
@@ -1,7 +1,20 @@
 <template>
     <div>
 
-        <h1 class="font-weight-light">Lista de Tarefas</h1>
+        <div class="row">
+            <div class="col-sm-10">
+                <h1 class="font-weight-light">Lista de tarefas</h1>
+            </div>
+            <div class="col-sm-2">
+                <button 
+                    class="btn btn-primary btn-sm floar-right"
+                    @click="exibirFormulario = !exibirFormulario"
+                >
+                    <i class="fa fa-plus"></i>
+                    <span> Crear</span>
+                </button>
+            </div>
+        </div>
 
         <ul class="list-group" v-if="tarefas.length > 0">
             <TarefasListaIten
@@ -12,15 +25,22 @@
 
         <p v-else>Nenhuma tarefa criada.</p>
 
-        <TarefaSalvar />
+        <TarefaSalvar 
+            v-if="exibirFormulario"
+            @criar="criarTarefa"
+        />
 
     </div>
 </template>
 
 <script>
+import axios from 'axios'
 
+import config from '../config/config'
 import TarefaSalvar from './TarefaSalvar.vue'
 import TarefasListaIten from './TarefasListaIten.vue'
+import { timeout } from 'q';
+import { setTimeout } from 'timers';
 
 export default {
     components: {
@@ -29,12 +49,27 @@ export default {
     },
     data() {
         return {
-            tarefas: [
-                { id: 1, titulo: 'Aprender JavaScript', concluido: true },        
-                { id: 2, titulo: 'Aprender Vue', concluido: true },
-                { id: 3, titulo: 'Aprender Axios', concluido: false }
-            ]
+            tarefas: [],
+            exibirFormulario: false
         }
-    }
+    },
+    created(){
+        axios.get(`${config.apiURL}/tarefas`)
+            .then((response) => {
+                console.log('GET/tarefas', response)
+                this.tarefas = response.data
+            })
+    },
+    methods: {
+        criarTarefa( tarefa ){
+            axios.post(`${config.apiURL}/tarefas`, tarefa )
+                .then((response) => {
+                    console.log('POST/tarefas', response)
+                    this.tarefas.push(response.data)
+                    setTimeout( this.exibirFormulario = false, 300 )
+                    
+                })
+        }
+    } 
 }
 </script>
